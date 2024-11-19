ba2995d412e4fbb926b0aa64ce84d1849123e4b2
NITeshGitH <nvermacodes@gmail.com>
2024-11-19 15:55:11 +0530

    first commit

---
+++ src/main/java/dev/nitesh/movies/ReviewRepository.java
@@ -0,0 +1,9 @@
+package dev.nitesh.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+}
